43467c407742dbfd72b41ab7c53a07edd48227d2
Daniel Loomb <daniel@statsig.com>
2023-09-27 17:22:16 -0700

    update version-sync

--- config/version-sync.js
+++ config/version-sync.js
@@ -4,11 +4,31 @@ const mainVersion = require('../package.json').version;
 const glob = require('glob');
 const fs = require('fs');
 
+const PACKAGES = [
+  'dloomb-client-core',
+  'dloomb-client-extensions',
+  'dloomb-client-on-device-eval',
+  'dloomb-client-react-native',
+  'dloomb-client-react',
+  'dloomb-client-remote-server-eval',
+  'dloomb-client-sha256',
+  'dloomb-client-test-helpers',
+];
+
 glob.sync('./packages/**/package.json').forEach((location) => {
   const content = fs.readFileSync(location);
-  const updated = content
-    .toString()
-    .replace(/"version": ".*"/, `"version": "${mainVersion}"`);
+  const updated = [
+    // Top level version
+    [/"version": ".*"/, `"version": "${mainVersion}"`],
+
+    // Any sibling deps
+    ...PACKAGES.map((package) => [
+      new RegExp(`"${package}": ".*"`),
+      `"${package}": "^${mainVersion}"`,
+    ]),
+  ].reduce((acc, [find, replace]) => {
+    return acc.replace(find, replace);
+  }, content.toString());
 
   fs.writeFileSync(location, updated);
 });
