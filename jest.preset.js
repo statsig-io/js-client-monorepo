0d4a4fd7b215f7aa29266b91fc12b3831fc7ca2f
Daniel Loomb <daniel@statsig.com>
2023-09-28 22:03:39 -0700

    Fix test

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,8 @@
+const path = require('path');
+
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  setupFiles: [path.resolve(__dirname, './tools/scripts/jest-setup.js')],
+};
---
+++ tools/scripts/jest-setup.js
@@ -0,0 +1,1 @@
+require('reflect-metadata'); // Fix for Reflect.metadata test error https://github.com/nrwl/nx/issues/8905
