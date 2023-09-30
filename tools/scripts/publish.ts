3e21f71d5adc8a93aec2ab138098b57aa4183841
Daniel Loomb <daniel@statsig.com>
2023-09-29 20:48:14 -0700

    chore: add ability to publish all packages

--- tools/scripts/publish.ts
+++ tools/scripts/publish.ts
@@ -1,1 +1,24 @@
-console.log('Foo');
+import { execSync } from 'child_process';
+import { existsSync } from 'fs';
+import path from 'path';
+
+const [, , packageName] = process.argv;
+
+if (typeof packageName !== 'string') {
+  throw new Error(`Invalid package name: ${packageName}`);
+}
+
+const root = path.resolve(__dirname, '../../');
+const dir = `${root}/dist/packages/${packageName}`;
+const configPath = `${dir}/package.json`;
+
+if (!existsSync(configPath)) {
+  throw new Error(`Failed to find package.json at ${configPath}`);
+}
+
+execSync(
+  `npm publish --tag beta --registry=https://registry.npmjs.org/ --userconfig=${root}/.npmrc --access public`,
+  { cwd: dir },
+);
+
+console.log('Foo', dir);
