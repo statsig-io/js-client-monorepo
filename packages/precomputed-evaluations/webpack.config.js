c55af77f8f6e01cb32fb85e546f58062f28d07e8
Daniel Loomb <daniel@statsig.com>
2023-10-01 09:49:47 -0700

    fix: remove rn async from precomp min js

--- packages/precomputed-evaluations/webpack.config.js
+++ packages/precomputed-evaluations/webpack.config.js
@@ -1,33 +1,41 @@
 const { composePlugins, withNx, withWeb } = require('@nx/webpack');
 const path = require('path');
 
-module.exports = composePlugins(withNx(), withWeb(), () => {
-  return {
-    entry: ['./dist/packages/precomputed-evaluations/src/index.js'],
-    mode: 'production',
-    target: 'web',
-    resolve: {
-      alias: {
-        '@sigstat/core': path.resolve(__dirname, '../../dist/packages/core'),
-      },
-      extensions: ['.js'],
-    },
-    output: {
-      filename: 'precomputed-evalutions.min.js',
-      library: {
-        type: 'umd',
-        name: {
-          root: 'statsig',
-          amd: 'statsig',
-          commonjs: 'statsig',
+module.exports = composePlugins(withNx(), withWeb(), () =>
+  // config
+  {
+    return {
+      // Uncomment if you want to use webpack-bundle-analyzer
+      // plugins: config.plugins.filter(
+      //   (x) => x.constructor.name === 'StatsJsonPlugin',
+      // ),
+      entry: ['./dist/packages/precomputed-evaluations/src/index.js'],
+      mode: 'production',
+      target: 'web',
+      resolve: {
+        alias: {
+          '@sigstat/core': path.resolve(__dirname, '../../dist/packages/core'),
         },
+        extensions: ['.js'],
       },
-      path: path.resolve(
-        __dirname,
-        '../../dist/packages/precomputed-evaluations/build',
-      ),
-      libraryExport: 'default',
-      globalObject: 'this',
-    },
-  };
-});
+      externals: ['@react-native-async-storage/async-storage'],
+      output: {
+        filename: 'precomputed-evalutions.min.js',
+        library: {
+          type: 'umd',
+          name: {
+            root: 'statsig',
+            amd: 'statsig',
+            commonjs: 'statsig',
+          },
+        },
+        path: path.resolve(
+          __dirname,
+          '../../dist/packages/precomputed-evaluations/build',
+        ),
+        libraryExport: 'default',
+        globalObject: 'this',
+      },
+    };
+  },
+);
