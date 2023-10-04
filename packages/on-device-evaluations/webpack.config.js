2e0f963c916a2970156489c2a39545783fbef39d
Daniel Loomb <daniel@statsig.com>
2023-10-04 10:28:49 -0700

    tool: add webpack to on-device-eval package

---
+++ packages/on-device-evaluations/webpack.config.js
@@ -0,0 +1,54 @@
+const { composePlugins, withNx, withWeb } = require('@nx/webpack');
+const path = require('path');
+
+module.exports = composePlugins(withNx(), withWeb(), () =>
+  // config
+  {
+    return {
+      // Uncomment if you want to use webpack-bundle-analyzer
+      // plugins: config.plugins.filter(
+      //   (x) => x.constructor.name === 'StatsJsonPlugin',
+      // ),
+      stats: {
+        assets: true,
+        modules: false,
+        colors: true,
+      },
+      entry: ['./dist/packages/on-device-evaluations/src/index.js'],
+      mode: 'production',
+      target: 'web',
+      resolve: {
+        alias: {
+          '@sigstat/core': path.resolve(__dirname, '../../dist/packages/core'),
+          '@sigstat/sha256': path.resolve(
+            __dirname,
+            '../../dist/packages/sha256',
+          ),
+        },
+        extensions: ['.js'],
+      },
+      externals: ['@react-native-async-storage/async-storage'],
+      output: {
+        filename: 'on-device-evaluations.min.js',
+        library: {
+          type: 'umd',
+          name: {
+            root: 'statsig',
+            amd: 'statsig',
+            commonjs: 'statsig',
+          },
+        },
+        path: path.resolve(
+          __dirname,
+          '../../dist/packages/on-device-evaluations/build',
+        ),
+        libraryExport: 'default',
+        globalObject: 'this',
+      },
+      performance: {
+        maxEntrypointSize: 50000,
+        hints: 'error',
+      },
+    };
+  },
+);
