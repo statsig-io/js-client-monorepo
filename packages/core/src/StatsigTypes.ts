894d26883df64b2a67f4b383c0d0a24ceb2267a3
Daniel Loomb <daniel@statsig.com>
2024-01-30 13:31:31 -0800

    chore: disable Flatten type warning

--- packages/core/src/StatsigTypes.ts
+++ packages/core/src/StatsigTypes.ts
@@ -2,6 +2,9 @@ const DEFAULT_RULE = 'default';
 
 export type Flatten<T> = {
   [K in keyof T]: T[K];
+
+  // Intentional: This is a utility type
+  // eslint-disable-next-line @typescript-eslint/ban-types
 } & {};
 
 export type StatsigOptionsCommon = {
