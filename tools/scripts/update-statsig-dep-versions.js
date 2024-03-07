e236b272da3588e7fb20722616909bdda26bd4e8
Daniel Loomb <daniel@statsig.com>
2024-03-06 20:43:58 -0800

    tool: sync dep versions

---
+++ tools/scripts/update-statsig-dep-versions.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const { version } = require('../../package.json');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+function sync(subpath) {
+  const filepath = path.resolve(__dirname, '../../', subpath);
+
+  let content = fs.readFileSync(filepath).toString();
+
+  const regexes = [
+    '@statsig/client-core',
+    '@statsig/sha256',
+    '@statsig/react-bindings',
+    '@statsig/precomputed-evaluations',
+    '@statsig/on-device-evaluations',
+  ].map((mod) => new RegExp(`(${mod}": ")([^"]*)`));
+
+  regexes.forEach((regex) => {
+    content = content.replace(regex, `$1${version}`);
+  });
+
+  fs.writeFileSync(filepath, content);
+}
+
+const files = execSync(
+  'find packages -type f -name "package.json" ! -path "*/node_modules/*"',
+)
+  .toString()
+  .trim()
+  .split('\n');
+files.forEach((subpath) => sync(subpath));
