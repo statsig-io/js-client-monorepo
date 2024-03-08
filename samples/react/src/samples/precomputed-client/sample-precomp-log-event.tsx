c32b68d026f25a71d03294fef5bd8df5d418beef
Daniel Loomb <daniel@statsig.com>
2024-03-08 14:50:26 -0800

    chore: tweak log-event snippet

--- samples/react/src/samples/precomputed-client/sample-precomp-log-event.tsx
+++ samples/react/src/samples/precomputed-client/sample-precomp-log-event.tsx
@@ -1,16 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+// <snippet>
+import type { StatsigEvent } from '@statsig/client-core';
+
+// </snippet>
 import { myStatsigClient } from './sample-precomp-instance';
 
 // prettier-ignore
 export default async function Sample(): Promise<void> {
 // <snippet>
-myStatsigClient.logEvent({
+const myEvent: StatsigEvent = {
   eventName: 'add_to_cart',
   value: 'SKU_12345',
   metadata: {
     price: '9.99',
     item_name: 'diet_coke_48_pack',
   },
-});
+}
+
+myStatsigClient.logEvent(myEvent);
 // </snippet>
 }
