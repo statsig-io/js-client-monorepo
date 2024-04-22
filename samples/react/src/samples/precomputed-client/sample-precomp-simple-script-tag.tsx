92eb89e16438f7b0e36f76c0a281f4d90ef7c966
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2024-04-22 15:08:04 -0700

    feat: document simple script tag auto init (#126)

---
+++ samples/react/src/samples/precomputed-client/sample-precomp-simple-script-tag.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+
+// prettier-ignore
+export default async function Sample(): Promise<void> {
+  (
+<div>
+{/* <snippet> */}
+<script src="https://cdn.jsdelivr.net/npm/@statsig/js-client@latest/build/statsig-js-client+session-replay+web-analytics.min.js?sdkKey=[YOUR_CLIENT_KEY]"></script>
+{/* </snippet> */}
+</div>
+  );
+  
+}
