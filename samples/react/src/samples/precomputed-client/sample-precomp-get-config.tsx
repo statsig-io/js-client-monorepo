5631a14b2d534cd06c917255aa432eb13b4d2c1c
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2024-05-30 13:26:44 -0700

    chore: use typed getter in snippets

--- samples/react/src/samples/precomputed-client/sample-precomp-get-config.tsx
+++ samples/react/src/samples/precomputed-client/sample-precomp-get-config.tsx
@@ -5,7 +5,7 @@ import { myStatsigClient } from './sample-precomp-instance';
 export default async function Sample(): Promise<void> {
 // <snippet>
 const dynamicConfig = myStatsigClient.getDynamicConfig("awesome_product_details");
-const itemName = dynamicConfig.value["product_name"] ?? "Some Fallback";
+const itemName = dynamicConfig.get("product_name", "Some Fallback");
 const price = dynamicConfig.value["price"] ?? 10.0;
 
 if (dynamicConfig.value["is_discount_enabled"] === true) {
