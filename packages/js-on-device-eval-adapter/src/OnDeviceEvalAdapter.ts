f8f186c04d5957ad96659e37d1122a6acd6db433
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2025-02-03 15:19:55 -0800

    feat: add ability to bootstrap OnDeviceEvalAdapter (#426)

--- packages/js-on-device-eval-adapter/src/OnDeviceEvalAdapter.ts
+++ packages/js-on-device-eval-adapter/src/OnDeviceEvalAdapter.ts
@@ -8,20 +8,45 @@ import {
   Layer,
   LayerEvaluationOptions,
   OverrideAdapter,
+  SpecsDataAdapter,
   StatsigUser,
+  StatsigUserInternal,
+  _makeFeatureGate,
 } from '@statsig/client-core';
+import { Evaluator, SpecStore } from '@statsig/on-device-eval-core';
+
+import { StatsigLocalSpecsDataAdapter } from './StatsigLocalSpecsDataAdapter';
 
 export class OnDeviceEvalAdapter implements OverrideAdapter {
-  constructor(private readonly onDeviceEval: Record<string, unknown>) {}
+  private _dataAdapter: SpecsDataAdapter;
+  private _store: SpecStore;
+  private _evaluator: Evaluator;
 
-  setData(data: string): void {}
+  constructor() {
+    this._dataAdapter = new StatsigLocalSpecsDataAdapter();
+    this._store = new SpecStore();
+    this._evaluator = new Evaluator(this._store);
+  }
+
+  setData(data: string): void {
+    this._dataAdapter.setData(data);
+    const result = this._dataAdapter.getDataSync();
+    this._store.setValuesFromDataAdapter(result);
+  }
 
   getGateOverride?(
     current: FeatureGate,
-    user: StatsigUser,
-    options?: FeatureGateEvaluationOptions,
+    user: StatsigUserInternal,
+    _options?: FeatureGateEvaluationOptions,
   ): FeatureGate | null {
-    return null;
+    if (current.details.reason.includes('Network')) {
+      return null;
+    }
+
+    const name = current.name;
+    const { evaluation, details } = this._evaluator.evaluateGate(name, user);
+    details.reason = '[OnDevice]' + details.reason;
+    return _makeFeatureGate(name, details, evaluation);
   }
 
   getDynamicConfigOverride?(
