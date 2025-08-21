4fc782073a7d4cd248914d35c28c7cba718215e5
Andre Terron <andre@statsig.com>
2025-08-21 14:01:22 -0700

    fix: move back to node legacy for bootstrap provider to enable webpac… (#618)
    * fix: move back to node legacy for bootstrap provider to enable webpacking of react-server
    
    * chore: fix pnpm lock
    
    * chore: fix build error
    
    ---------
    
    Co-authored-by: Brock Lumbard <brock@statsig.com>

--- packages/react-server/src/StatsigBootstrapProvider.tsx
+++ packages/react-server/src/StatsigBootstrapProvider.tsx
@@ -1,27 +1,11 @@
-import * as React from 'react';
+import { JSX, ReactNode } from 'react';
+import Statsig, { StatsigUser as StatsigNodeUser } from 'statsig-node';
 
 import { StatsigUser } from '@statsig/react-bindings';
-import {
-  Statsig,
-  StatsigUser as StatsigUserCore,
-} from '@statsig/statsig-node-core';
 
 import BootstrapClientSubProvider from './BootstrapClientSubProvider';
 
-let isStatsigReady = false;
-
-function filterUndefined<T>(
-  obj: Record<string, T | undefined> | undefined,
-): Record<string, T> {
-  if (!obj) return {};
-  const result: Record<string, T> = {};
-  for (const [key, value] of Object.entries(obj)) {
-    if (value !== undefined) {
-      result[key] = value;
-    }
-  }
-  return result;
-}
+const statsigInitialization: Record<string, Promise<any> | undefined> = {};
 
 export default async function StatsigBootstrapProvider({
   children,
@@ -30,33 +14,19 @@ export default async function StatsigBootstrapProvider({
   serverKey,
 }: {
   user: StatsigUser;
-  children: React.ReactNode;
+  children: ReactNode;
   clientKey: string;
   serverKey: string;
-}): Promise<React.JSX.Element> {
-  if (!isStatsigReady) {
-    isStatsigReady = true;
-    const statsig = Statsig.newShared(serverKey);
-    await statsig.initialize();
+}): Promise<JSX.Element> {
+  if (!statsigInitialization[serverKey]) {
+    statsigInitialization[serverKey] = Statsig.initialize(serverKey);
   }
-
-  const sharedStatsig = Statsig.shared();
-  const userCore = new StatsigUserCore({
-    userID: user.userID,
-    customIDs: filterUndefined(user.customIDs),
-    email: user.email,
-    userAgent: user.userAgent,
-    custom: filterUndefined(user.custom),
-    country: user.country,
-    locale: user.locale,
-    appVersion: user.appVersion,
-    ip: user.ip,
-    privateAttributes: filterUndefined(user.privateAttributes ?? undefined),
-  });
-  const values = sharedStatsig.getClientInitializeResponse(userCore, {
-    hashAlgorithm: 'djb2',
-    clientSdkKey: clientKey,
-  });
+  await statsigInitialization[serverKey];
+  const values = JSON.stringify(
+    Statsig.getClientInitializeResponse(user as StatsigNodeUser, clientKey, {
+      hash: 'djb2',
+    }),
+  );
 
   return (
     <BootstrapClientSubProvider
