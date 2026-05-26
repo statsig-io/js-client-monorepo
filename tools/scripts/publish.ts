4c6cde2677fe71cd75c623c059ec15fb62c40939
Daniel Graham <daniel.graham@amplitude.com>
2026-05-26 13:40:06 -0700

    chore: update release_bot.yml to work with trusted publishing (#885)

--- tools/scripts/publish.ts
+++ tools/scripts/publish.ts
@@ -12,11 +12,7 @@ const root = path.resolve(__dirname, '../../');
 const dir = `${root}/dist/packages/${packageName}`;
 const configPath = `${dir}/package.json`;
 
-if (!existsSync(`${root}/.npmrc`)) {
-  // put a .npmrc file in the root sdk directory
-  // should look like: //registry.npmjs.org/:_authToken=npm_YOUR_TOKEN_TO_PUBLISH
-  throw new Error(`.npmrc file not found it root directory`);
-}
+const hasRcFile = existsSync(`${root}/.npmrc`);
 
 if (!existsSync(configPath)) {
   throw new Error(`Failed to find package.json at ${configPath}`);
@@ -27,10 +23,10 @@ const isBeta = false;
 const publish = [
   `npm publish`,
   `--registry=https://registry.npmjs.org/`,
-  `--userconfig=${root}/.npmrc`,
+  hasRcFile ? `--userconfig=${root}/.npmrc` : '',
   `--access public`,
   isBeta ? `--tag beta` : '',
-];
+].filter(Boolean);
 
 // If you need to promote a version to latest
 // const promote = [
